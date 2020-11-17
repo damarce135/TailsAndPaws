f3f67f5d3b3c6b68408d69cf07758f99312d2ee1
Marcela Ortega <damarce135@gmail.com>
2020-11-16 22:51:56 -0600

    Added calendar option and calendario table to DB

--- TailsP/FrontEnd/Controllers/HomeController.cs
+++ TailsP/FrontEnd/Controllers/HomeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BackEnd.Entities;
 
 namespace FrontEnd.Controllers
 {
+
     [Authorize(Roles = "Administrador,Encargado")]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+           
+            return View(TPEntities.Calendario.ToList());
         }
 
         public ActionResult About()
@@ -27,5 +30,58 @@ namespace FrontEnd.Controllers
 
             return View();
         }
+
+        public JsonResult GetEvents()
+        {
+            
+                var events = Calendario.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Calendario e)
+        {
+            var status = false;
+                if (e.idCalendario > 0)
+                {
+                    //Update the event
+                    var v = Calendario.Where(a => a.idCalendario == e.idCalendario).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.asunto;
+                        v.Start = e.fechaInicio;
+                        v.End = e.fechaFinal;
+                        v.Description = e.descripcion;
+                        v.IsFullDay = e.diaCompleto;
+                        v.ThemeColor = e.temaColor;
+                    }
+                }
+                else
+                {
+                    Calendario.Add(e);
+                }
+                SaveChanges();
+                status = true;
+            
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+      
+                var v = Calendario.Where(a => a.idCalendario == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    Calendario.Remove(v);
+                    SaveChanges();
+                    status = true;
+                }
+            
+            return new JsonResult { Data = new { status = status } };
+        } 
+
     }
 }
