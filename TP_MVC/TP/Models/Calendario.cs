0a8fc81bdf334eb3db29aa9629d3c4992da43cb4
Marcela Ortega <damarce135@gmail.com>
2020-11-30 23:01:38 -0600

    Update - changed to spanish Donante, Home, Producto and Organizacion

--- TP_MVC/TP/Models/Calendario.cs
+++ TP_MVC/TP/Models/Calendario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using TP.Validations;
 
 #nullable disable
 
@@ -9,12 +10,29 @@ namespace TP.Models
     public partial class Calendario
     {
         [Key]
+        [Display(Name = "Idntificador")]
         public int IdCalendario { get; set; }
-        public string Asunto { get; set; }
+
+        [GenericRequired]
+        [Display(Name = "Asunto")]
+        public string? Asunto { get; set; }
+
+        [GenericRequired]
+        [Display(Name = "Inicio")]
         public DateTime FechaInicio { get; set; }
+
+        [Display(Name = "Final")]
         public DateTime FechaFinal { get; set; }
-        public string Descripcion { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string? Descripcion { get; set; }
+
+        [GenericRequired]
+        [Display(Name = "Evento de todo el día")]
         public bool? DiaCompleto { get; set; }
-        public string TemaColor { get; set; }
+
+        [GenericRequired]
+        [Display(Name = "Color")]
+        public string? TemaColor { get; set; }
     }
 }
